49a7d07afcdcec62c7970c88a6044250fc70cdf9
Samet Acar <smtacar13@gmail.com>
2022-03-09 15:31:09 +0100

    last class added

--- src/test/java/apitests/hrApiParameterTest.java
+++ src/test/java/apitests/hrApiParameterTest.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,8 +18,29 @@ public class hrApiParameterTest {
 
     @BeforeClass
     public void beforeClass(){
-        baseURI="http://18.212.211.128:8000";
+        baseURI= ConfigurationReader.get("hr_api_url");
     }
+    /*
+        Given accept type is Json
+        And parameters: q = {"region_id":2}
+        When users sends a GET request to "/countries"
+        Then status code is 200
+        And Content type is application/json
+        And Payload should contain "United States of America"
+        {"region_id":2}
+     */
+    @Test
+    public void test(){
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"region_id\":2}")
+                .when().get("countries");
+        assertEquals(response.statusCode(),200);
+        assertEquals(response.contentType(),"application/json");
+        assertTrue(response.body().asString().contains("United States of America"));
+
+
+    }
+
 
 
 }
